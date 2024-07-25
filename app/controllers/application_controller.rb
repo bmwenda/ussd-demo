aea9f990bafbe3765f4642a136313241aea84334
Boniface Ntarangwi <boniface.ntarangwi@engie.com>
2024-07-25 14:27:16 +0300

    Add status controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+
+  def ping
+    render plain: "All good!\n POST to /callback to see menu options", status: 200
+  end
 end
